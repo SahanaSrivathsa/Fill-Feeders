3127f2774fb2325686f8e35fe8f6627dc4e680b8
Sahana Srivathsa <sahana.srivathsa@gmail.com>
2019-05-06 14:56:51 -0700

    Add project files.

---
+++ Fill_Feeders/FeederGui.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Fill_Feeders
+{
+    partial class FeederGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FeederGui));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.close3 = new System.Windows.Forms.Button();
+            this.close4 = new System.Windows.Forms.Button();
+            this.close1 = new System.Windows.Forms.Button();
+            this.close2 = new System.Windows.Forms.Button();
+            this.fill1 = new System.Windows.Forms.Button();
+            this.fill2 = new System.Windows.Forms.Button();
+            this.fill3 = new System.Windows.Forms.Button();
+            this.fill4 = new System.Windows.Forms.Button();
+            this.done_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(683, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // close3
+            // 
+            this.close3.BackColor = System.Drawing.Color.Red;
+            this.close3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close3.Location = new System.Drawing.Point(99, 45);
+            this.close3.Name = "close3";
+            this.close3.Size = new System.Drawing.Size(70, 54);
+            this.close3.TabIndex = 1;
+            this.close3.Text = "STOP";
+            this.close3.UseVisualStyleBackColor = false;
+            this.close3.Click += new System.EventHandler(this.close3_Click);
+            // 
+            // close4
+            // 
+            this.close4.BackColor = System.Drawing.Color.Red;
+            this.close4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close4.Location = new System.Drawing.Point(304, 355);
+            this.close4.Name = "close4";
+            this.close4.Size = new System.Drawing.Size(80, 65);
+            this.close4.TabIndex = 2;
+            this.close4.Text = "STOP";
+            this.close4.UseVisualStyleBackColor = false;
+            this.close4.Click += new System.EventHandler(this.close4_Click);
+            // 
+            // close1
+            // 
+            this.close1.BackColor = System.Drawing.Color.Red;
+            this.close1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close1.Location = new System.Drawing.Point(513, 45);
+            this.close1.Name = "close1";
+            this.close1.Size = new System.Drawing.Size(70, 54);
+            this.close1.TabIndex = 3;
+            this.close1.Text = "STOP";
+            this.close1.UseVisualStyleBackColor = false;
+            this.close1.Click += new System.EventHandler(this.close1_Click);
+            // 
+            // close2
+            // 
+            this.close2.BackColor = System.Drawing.Color.Red;
+            this.close2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close2.Location = new System.Drawing.Point(306, 45);
+            this.close2.Name = "close2";
+            this.close2.Size = new System.Drawing.Size(70, 54);
+            this.close2.TabIndex = 4;
+            this.close2.Text = "STOP";
+            this.close2.UseVisualStyleBackColor = false;
+            this.close2.Click += new System.EventHandler(this.close2_Click);
+            // 
+            // fill1
+            // 
+            this.fill1.BackColor = System.Drawing.Color.CadetBlue;
+            this.fill1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fill1.Location = new System.Drawing.Point(513, 45);
+            this.fill1.Name = "fill1";
+            this.fill1.Size = new System.Drawing.Size(70, 54);
+            this.fill1.TabIndex = 5;
+            this.fill1.Text = "Fill 1";
+            this.fill1.UseVisualStyleBackColor = false;
+            this.fill1.Click += new System.EventHandler(this.fill1_Click);
+            // 
+            // fill2
+            // 
+            this.fill2.BackColor = System.Drawing.Color.CadetBlue;
+            this.fill2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fill2.Location = new System.Drawing.Point(304, 45);
+            this.fill2.Name = "fill2";
+            this.fill2.Size = new System.Drawing.Size(70, 54);
+            this.fill2.TabIndex = 6;
+            this.fill2.Text = "Fill 2";
+            this.fill2.UseVisualStyleBackColor = false;
+            this.fill2.Click += new System.EventHandler(this.fill2_Click);
+            // 
+            // fill3
+            // 
+            this.fill3.BackColor = System.Drawing.Color.CadetBlue;
+            this.fill3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fill3.Location = new System.Drawing.Point(99, 45);
+            this.fill3.Name = "fill3";
+            this.fill3.Size = new System.Drawing.Size(70, 54);
+            this.fill3.TabIndex = 7;
+            this.fill3.Text = "Fill 3";
+            this.fill3.UseVisualStyleBackColor = false;
+            this.fill3.Click += new System.EventHandler(this.fill3_Click);
+            // 
+            // fill4
+            // 
+            this.fill4.BackColor = System.Drawing.Color.CadetBlue;
+            this.fill4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fill4.Location = new System.Drawing.Point(306, 355);
+            this.fill4.Name = "fill4";
+            this.fill4.Size = new System.Drawing.Size(78, 65);
+            this.fill4.TabIndex = 8;
+            this.fill4.Text = "Fill 4";
+            this.fill4.UseVisualStyleBackColor = false;
+            this.fill4.Click += new System.EventHandler(this.fill4_Click);
+            // 
+            // done_btn
+            // 
+            this.done_btn.BackColor = System.Drawing.Color.SkyBlue;
+            this.done_btn.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.done_btn.Location = new System.Drawing.Point(595, 385);
+            this.done_btn.Name = "done_btn";
+            this.done_btn.Size = new System.Drawing.Size(70, 54);
+            this.done_btn.TabIndex = 9;
+            this.done_btn.Text = "Done";
+            this.done_btn.UseVisualStyleBackColor = false;
+            this.done_btn.Click += new System.EventHandler(this.done_btn_Click);
+            // 
+            // FeederGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 465);
+            this.Controls.Add(this.done_btn);
+            this.Controls.Add(this.fill4);
+            this.Controls.Add(this.fill3);
+            this.Controls.Add(this.fill2);
+            this.Controls.Add(this.fill1);
+            this.Controls.Add(this.close2);
+            this.Controls.Add(this.close1);
+            this.Controls.Add(this.close4);
+            this.Controls.Add(this.close3);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FeederGui";
+            this.Text = "Fill Feeders";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button close3;
+        private System.Windows.Forms.Button close4;
+        private System.Windows.Forms.Button close1;
+        private System.Windows.Forms.Button close2;
+        private System.Windows.Forms.Button fill1;
+        private System.Windows.Forms.Button fill2;
+        private System.Windows.Forms.Button fill3;
+        private System.Windows.Forms.Button fill4;
+        private System.Windows.Forms.Button done_btn;
+    }
+}
+
---
+++ Fill_Feeders/FeederGui.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace Fill_Feeders
+{
+    public partial class FeederGui : Form
+    {
+        private static readonly SerialPort serialPort = new SerialPort();
+        public BackgroundWorker felix = new BackgroundWorker();
+        public FeederGui()
+        {
+            
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM3";
+            serialPort.ReadTimeout = 10000;
+            serialPort.Encoding = Encoding.UTF8;
+            serialPort.DiscardNull = true;
+            serialPort.WriteBufferSize = 10000;
+            serialPort.Open();
+
+            serialPort.DiscardInBuffer();
+            serialPort.DiscardOutBuffer();
+            InitializeComponent();
+        }
+        public void listen_to_arduino(object sender, DoWorkEventArgs e)
+        //The "listener" that is the mediator between the worker (Felix) and the updater
+        {
+            try
+            {
+                var changedData = serialPort.ReadTo("\n");
+                e.Result = changedData;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public static void sendMessage(string button) //handles messages to be sent to the UNO for filling/cleaning
+        {
+            switch (button)
+            {
+                case "1":
+                    try
+                    {
+                        serialPort.Write(new[] { 'X' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "2":
+                    try
+                    {
+                        serialPort.Write(new[] { 'Y' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "3":
+                    try
+                    {
+                        serialPort.Write(new[] { 'Z' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "4":
+                    try
+                    {
+                        serialPort.Write(new[] { 'W' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "11":
+                    try
+                    {
+                        serialPort.Write(new[] { 'x' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "22":
+                    try
+                    {
+                        serialPort.Write(new[] { 'y' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "33":
+                    try
+                    {
+                        serialPort.Write(new[] { 'z' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+                case "44":
+                    try
+                    {
+                        serialPort.Write(new[] { 'w' }, 0, 1);
+                    }
+                    catch (Exception)
+                    {
+                        ;
+                    }
+                    break;
+            }
+        }
+
+        private void close3_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("33");
+            fill3.Show();
+        }
+
+        private void close2_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("22");
+            fill2.Show();
+        }
+
+        private void close1_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("11");
+            fill1.Show();
+        }
+
+        private void close4_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("44");
+            fill4.Show();
+        }
+
+        private void fill3_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("3");
+            fill3.Hide();
+        }
+
+        private void fill2_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("2");
+            fill2.Hide();
+        }
+
+        private void fill1_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("1");
+            fill1.Hide();
+        }
+
+        private void fill4_Click(object sender, EventArgs e)
+        {
+            FeederGui.sendMessage("4");
+            fill4.Hide();
+        }
+
+        private void done_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+
+
